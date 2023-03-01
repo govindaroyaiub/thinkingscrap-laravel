f5ffbb0235f659cebe36f697b72c9ca88b044573
Govinda Roy <43463520+govindaroyaiub@users.noreply.github.com>
2023-03-01 13:35:07 +0600

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="{{ $data['meta_description'] }}">
+    <meta name="description" content="{{ $companyData['meta_description'] }}">
     <meta name="author" content="Thinking Scrap">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Thinking Scrap</title>
